98d676db2ce75f5954df241735ee7a1c4d721555
Nanotorre <fernandodelatorre4@gmail.com>
2019-10-14 19:28:17 +0200

    dotenvs changed

---
+++ server/public/precache-manifest.9115346a9468511398f76ccddcbe159a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e6df12ef95056d8655f8771179358d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "219aa2c5a87b0c832389",
+    "url": "/static/css/main.49259d75.chunk.css"
+  },
+  {
+    "revision": "c3a9727daf2cc4175b50",
+    "url": "/static/js/2.23e2c91e.chunk.js"
+  },
+  {
+    "revision": "219aa2c5a87b0c832389",
+    "url": "/static/js/main.7e568f12.chunk.js"
+  },
+  {
+    "revision": "df00b4c9814429a119cc",
+    "url": "/static/js/runtime-main.4865f1fb.js"
+  },
+  {
+    "revision": "44f34020efe7c36cc5567c71b7c4430a",
+    "url": "/static/media/box.44f34020.svg"
+  }
+]);
