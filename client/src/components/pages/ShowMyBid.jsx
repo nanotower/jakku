0e88dbc67a3c69beb8020a924484191a87115832
Nanotorre <fernandodelatorre4@gmail.com>
2019-10-20 23:15:41 +0200

    del but no update without reload

--- client/src/components/pages/ShowMyBid.jsx
+++ client/src/components/pages/ShowMyBid.jsx
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Bidmapcontainer from "../molecules/Productmapcontainer";
-import { Row, Card, CardTitle, Col, Preloader} from 'react-materialize';
+import { Row, Card, CardTitle, Col, Preloader } from "react-materialize";
 import AllProducts from "../organisms/AllProducts";
 import ButtonAdd from "../atoms/ButtonAdd";
 import CollapsibleProducs from "../molecules/CollapsibleProducs";
 import RoutesService from "../../RoutesService";
 import PreloaderSpinner from "../atoms/PreloaderSpinner";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 import moment from "moment";
-import 'moment/locale/es'
-moment.locale('es')
+import "moment/locale/es";
+import DelModalWarning from "../molecules/DelModalWarning";
+moment.locale("es");
 
 class ShowMyBid extends Component {
   constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       user: this.props.user,
       product: this.props.user.products
-    }
-    this.router = new RoutesService()
+    };
+    this.router = new RoutesService();
   }
-  componentDidMount=() =>{
-
+  componentDidMount = () => {
     // this.setState({
     //   ...this.state,
     //   products:this.props.user.products
@@ -35,34 +35,34 @@ class ShowMyBid extends Component {
     //   });
     // });
     // console.log(this.state)
-    this.props.fromApp()
-    .then(()=> {
+    this.props.fromApp().then(() => {
       this.setState({
         ...this.state,
         user: this.props.user,
         products: this.props.user.products
-      })
-    })
-
-  }
-  deleteProduct = (id) => {
-    console.log("borrando")
-    this.route.deleteProduct(id)
-    .then(user => {
+      });
+    });
+  };
+  deleteProduct = id => {
+    
+    console.log(id);
+    console.log("borrando");
+    this.router.deleteProduct(id).then(user => {
       this.setState({
         ...this.state,
-        user
-      })
-    })
-  }
-  goBack =()=> {
-    this.props.history.push("/")
-  }
+        user: user,
+        products: user.products
+      });
+    });
+  };
+  goBack = () => {
+    this.props.history.push("/");
+  };
   transformDate = () => {
-    moment.lang('es');
-    const dateTransformed= moment(this.props.user.bid.deadLine).format('LL')
-    return <p>{dateTransformed}</p>
-  }
+    moment.lang("es");
+    const dateTransformed = moment(this.props.user.bid.deadLine).format("LL");
+    return <p>{dateTransformed}</p>;
+  };
   extractCoordinates(latlng) {
     const lat = latlng[0];
     const lng = latlng[1];
@@ -70,53 +70,48 @@ class ShowMyBid extends Component {
   }
 
   render() {
-    if(this.props.user.bid ) {
-      if(this.state.user.products) {
+    if (this.props.user.bid) {
+      if (this.state.user.products) {
         return (
-         
           <div className="your-bid-container">
             <div className="top-box">
-            <a className="back-btn"><i class="medium material-icons" onClick={this.goBack}>arrow_back</i></a>
-          <h1>PANEL DE CONTROL DE TU MUDANZA</h1>
-          <p>
-            Recogerán las cosas el día <span>{this.transformDate()}</span> entre las
-            <span>{this.props.user.bid.from}</span> y las <span>{this.props.user.bid.to}</span>
-          </p>
-          
-          <ButtonAdd product={true}></ButtonAdd>
-          <h2>TUS CAJAS</h2>
+              <a className="back-btn">
+                <i class="medium material-icons" onClick={this.goBack}>
+                  arrow_back
+                </i>
+              </a>
+              <h1>PANEL DE CONTROL DE TU MUDANZA</h1>
+              <p>
+                Recogerán las cosas el día <span>{this.transformDate()}</span>{" "}
+                entre las
+                <span>{this.props.user.bid.from}</span> y las{" "}
+                <span>{this.props.user.bid.to}</span>
+              </p>
+
+              <ButtonAdd
+                product={true}
+                passText="Ya no vendo esta caja"
+              ></ButtonAdd>
+              <h2>TUS CAJAS</h2>
             </div>
-           
-        
-          
-          <CollapsibleProducs 
-          products={this.state.products} user={this.state.user}
-          deleteFromShow={(id)=>this.deleteProduct(id)}
-          ></CollapsibleProducs>
-         
-    
-        
+            <CollapsibleProducs
+              products={this.state.products}
+              user={this.state.user}
+              deleteFromShow={id => this.deleteProduct(id)}
+            ></CollapsibleProducs>
           </div>
-
-          )
+        );
+      } else {
+        return <PreloaderSpinner></PreloaderSpinner>;
       }
-      else {
-        return (
-          <PreloaderSpinner></PreloaderSpinner>
-        )
-
-      }
-      
-    }
-    else {
-      return(
+    } else {
+      return (
         <div className="your-bid-container">
-        <p>No tienes ninguna mudanza creada.</p>
-        <ButtonAdd bid={true}></ButtonAdd>
+          <p>No tienes ninguna mudanza creada.</p>
+          <ButtonAdd bid={true}></ButtonAdd>
         </div>
-      )
+      );
     }
   }
-
 }
-export default withRouter(ShowMyBid)
+export default withRouter(ShowMyBid);
